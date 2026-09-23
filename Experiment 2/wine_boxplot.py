e6454f65fe16023b5b4b633e3ddb69fad64026e2
Anjali Kumari <anjalikumariakri08@gmail.com>
2026-09-23 18:17:59 +0530

    Update wine_boxplot.py

--- Experiment 2/wine_boxplot.py
+++ Experiment 2/wine_boxplot.py
@@ -2,20 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.datasets import load_wine
-
-# Load Wine dataset
 wine = load_wine()
-
-# Create DataFrame
 df = pd.DataFrame(wine.data, columns=wine.feature_names)
-
-# Plot boxplots
 plt.figure(figsize=(12, 6))
 sns.boxplot(data=df)
-
 plt.title("Boxplots of Wine Dataset")
 plt.xticks(rotation=90)
 plt.tight_layout()
-
 plt.savefig("wine_boxplots.png")
 plt.show()
